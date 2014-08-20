1b357ca3880f0eccec293ace258b5fc64f70f1b4
Brian Upton <pair+brian@rally.org>
2014-08-20 14:31:08 -0700

    created new web-apps directory structure

---
+++ web-apps/app-boilerplate/app/app.js
@@ -0,0 +1,12 @@
+(function(){
+	'use strict';
+
+	angular.module('app-boilerplate', [ 'ngRoute','app-boilerplate-main','templates' ])
+	  .config(function ($routeProvider) {
+	    $routeProvider
+	      .otherwise({
+	        redirectTo: '/'
+	      });
+	  });
+	  
+})();
