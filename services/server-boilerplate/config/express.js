a2517e9fee3a69c0f48f023cfc9ed4c4cf504f91
Sonjaya Tandon <pair+sonjaya@rally.org>
2014-08-27 11:48:42 -0700

    resolved PR comment

--- services/server-boilerplate/config/express.js
+++ services/server-boilerplate/config/express.js
@@ -24,7 +24,6 @@ module.exports = function(app) {
 	app.use(function (req, res, next) {
 
 	    // Website you wish to allow to connect
-//	    res.setHeader('Access-Control-Allow-Origin', 'http://staging-greenchef-static.s3-website-us-west-2.amazonaws.com');
 	    res.setHeader('Access-Control-Allow-Origin', '*');
 
 	    // Request methods you wish to allow
